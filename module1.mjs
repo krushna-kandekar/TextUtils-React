b11040889dc385eb19703f0dd36fabd25c2f42c5
Krushna Kandekar <kandekarkrushna389@gmail.com>
2024-07-09 23:16:14 +0530

    remaning

---
+++ module1.mjs
@@ -0,0 +1,6 @@
+import dza, {b, c, d} from  './module2.mjs';
+console.log(dza);
+console.log(b);
+console.log(c);
+console.log(d);
+
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Krushna";
+const b = "Pratik";
+const c = "Pritesh";
+const d = "Vibhas";
+
+export default a;
+export{b};
+export{c};
+export{d};
